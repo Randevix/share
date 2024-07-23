93d9163aac4932a4b8ea4a035b333e3ce7914565
Randevix <s.aksoy@gmail.com>
2024-07-23 16:33:51 +0300

    Updated necessary files

--- app.js
+++ app.js
@@ -1,54 +1,18 @@
 require('dotenv').config({ path: '../.env' });
 const replserver = require('./utils/replserver');
 const whatsappService = require('./whatsapp/whatsappSocket');
-const SignalRClient = require('./soket/signalRClient');
+const soket = require('./soket/webSocket');
 const config = require('./config'); // Yapýlandýrma dosyasýný içe aktar
 
 global.w = new Object();
 replserver.start()
 whatsappService.start();
 
-const soket = new SignalRClient({
-    serverUrl: process.env.WEBSOCKET_SERVER || config.websocket.serverUrl,
-    hubs: [process.env.HUB_NAME || config.websocket.hubName],
-    startLater: true,
-    reconnectRetryDelayMs: 5000,
-    hearthbeatInterval: 30000,
-    queryString: { userId: -1, clientName: "Nodi", waConStatus: whatsappService.connection.status }
-});
-
-
-soket.client.on(soket.hubNames[0], 'clientsStatus', (message) => {
-    console.log('clientsStatus:', message);
-});
-
-soket.client.serviceHandlers.connected = async (connection) => {
-    soket.eventTrigger('connected', null, connection);
-
-
-};
-
-soket.client.serviceHandlers.messageReceived = (message) => {
-    soket.messageReceived(message);
-    return false;
-};
-
 soket.start().then(() => {
     console.log("SignalR client started.");
 }).catch(error => {
     console.error("Socket connection failed:", error);
 });
-
-soket.messageReceived = (message) => {
-    if (message.utf8Data === "{}") return;
-    console.log("Message received: ", message);
-
-};
-
-soket.incomingCommands = async (command) => {
-    console.log("Incoming command: ", command);
-
-};
 
 
 
