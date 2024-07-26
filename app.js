88b6f71874985c5bc4e1baadc0a9cfbef9ccdb43
Randevix <s.aksoy@gmail.com>
2024-07-26 14:49:06 +0300

    Updated necessary files

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const config = require('./config'); // Yapýlandýrma dosyasýný içe aktar
 
 global.w = new Object();
 replserver.start()
-whatsappService.start();
+if (config.connectWhatsappOnStart) whatsappService.start();
 
 
 
@@ -23,28 +23,4 @@ soket.start().then(() => {
 
 
 
-
-//     soket.incomingCommands = (command) => {
-//        console.log("Message from client: ", command);
-
-//        let response;
-//        try {
-//            switch (command.type) {
-//                case 'whatsapp':
-//                  //  response = await whatsappHandlers[command.action](command.data);
-//                    break;
-//                default:
-//                    response = { error: 'Invalid command type' };
-//                    break;
-//            }
-//        } catch (error) {
-//            response = { error: error.message };
-//        }
-
-//        this.client.call(this.client.hubs[0], 'toClient', response);
-//    }
-
-//}).catch(error => {
-//    console.error("Socket connection failed:", error);
-//});
 
--- soket/webSocket.js
+++ soket/webSocket.js
@@ -3,18 +3,27 @@ const whatsappService = require('../whatsapp/whatsappSocket');
 const SignalRClient = require('./signalRClient');
 const config = require('../config');
 
+const user = {
+    Id: config.websocket.userId,
+    firmId: config.websocket.firmId,
+    roleId: config.websocket.roleId
+};
+
 const soket = new SignalRClient({
     serverUrl: process.env.WEBSOCKET_SERVER || config.websocket.serverUrl,
     hubs: [process.env.HUB_NAME || config.websocket.hubName],
     startLater: true,
     reconnectRetryDelayMs: 5000,
     hearthbeatInterval: 30000,
-    queryString: { userId: -1, clientName: "Nodi", waConStatus: whatsappService.connection.status }
+    queryString: {
+        user: JSON.stringify(user)
+    }
 });
+;
 
 
-soket.client.on(soket.hubNames[0], 'clientsStatus', (message) => {
-    console.log('clientsStatus:', message);
+soket.client.on(soket.hubNames[0], 'clientsStatus', (message,id) => {
+    console.log('clientsStatus:', message,id);
 });
 
 soket.client.serviceHandlers.connected = async (connection) => {
@@ -24,7 +33,7 @@ soket.client.serviceHandlers.connected = async (connection) => {
 };
 
 soket.client.serviceHandlers.messageReceived = (message) => {
-    soket.messageReceived(message);
+    //soket.messageReceived(message);
     return false;
 };
 
